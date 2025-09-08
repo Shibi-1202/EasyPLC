a9c8961113ad7b99e20ca59db28b8464a787df17
Shibi <shibih.060412@gmail.com>
2025-09-08 13:30:44 +0530

    Connected frontend and backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Add this proxy configuration
+    proxy: {
+      // Proxy requests starting with '/generate-code'
+      '/generate-code': {
+        target: 'http://127.0.0.1:8000', // Your backend server URL
+        changeOrigin: true, // Needed for virtual hosted sites
+      }
+    }
+  }
 })
