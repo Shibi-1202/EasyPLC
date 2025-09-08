33c0e5792466f9d05d9650e9622a8d9845aaf331
Shibi <shibih.060412@gmail.com>
2025-09-08 14:00:44 +0530

    Connected frontend and backend3

--- frontend/src/components/Generator.jsx
+++ frontend/src/components/Generator.jsx
@@ -283,7 +283,7 @@ export default function Generator({ onResult, restore }) {
     setLadderRungs([]);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/generate", {
+      const response = await fetch("{https://easyplc.onrender.com}/generate", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ nl }),
